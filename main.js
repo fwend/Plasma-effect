2550ce9f4f68c8f999dfb3f1934e9032e4d46e03
fwend <josvanuden@gmail.com>
2021-12-14 12:43:16 +0100

    refactor

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-var canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 canvas.width = 700;
 canvas.height = 500;
 
-var g = canvas.getContext('2d');
+const g = canvas.getContext('2d');
 
-var plasma = createPlasma(canvas.width, canvas.height);
-var hueShift = 0;
+const plasma = createPlasma(canvas.width, canvas.height);
+let hueShift = 0;
 
 function createPlasma(w, h) {
-    var buffer = new Array(h);
+    const buffer = new Array(h);
 
-    for (var y = 0; y < h; y++) {
+    for (let y = 0; y < h; y++) {
         buffer[y] = new Array(w);
 
-        for (var x = 0; x < w; x++) {
+        for (let x = 0; x < w; x++) {
 
-            var value = Math.sin(x / 16.0);
+            let value = Math.sin(x / 16.0);
             value += Math.sin(y / 8.0);
             value += Math.sin((x + y) / 16.0);
             value += Math.sin(Math.sqrt(x * x + y * y) / 8.0);
@@ -29,15 +29,15 @@ function createPlasma(w, h) {
 }
 
 function drawPlasma(w, h) {
-    var img = g.getImageData(0, 0, w, h);
+    const img = g.getImageData(0, 0, w, h);
 
-    for (var y = 0; y < h; y++) {
+    for (let y = 0; y < h; y++) {
 
-        for (var x = 0; x < w; x++) {
+        for (let x = 0; x < w; x++) {
 
-            var hue = hueShift + plasma[y][x] % 1;
-            var rgb = HSVtoRGB(hue, 1, 1);
-            var pos = (y * w + x) * 4;
+            const hue = hueShift + plasma[y][x] % 1;
+            const rgb = HSVtoRGB(hue, 1, 1);
+            const pos = (y * w + x) * 4;
             img.data[pos] = rgb.r;
             img.data[pos + 1] = rgb.g;
             img.data[pos + 2] = rgb.b;
@@ -48,7 +48,7 @@ function drawPlasma(w, h) {
 
 /* copied from stackoverflow */
 function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
+    let r, g, b, i, f, p, q, t;
 
     i = Math.floor(h * 6);
     f = h * 6 - i;
@@ -56,12 +56,12 @@ function HSVtoRGB(h, s, v) {
     q = v * (1 - f * s);
     t = v * (1 - (1 - f) * s);
     switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
     }
     return {
         r: Math.round(r * 255),
@@ -77,8 +77,8 @@ function drawBorder() {
 }
 
 function animate(lastFrameTime) {
-    var time = new Date().getTime();
-    var delay = 42;
+    const time = new Date().getTime();
+    const delay = 42;
 
     if (lastFrameTime + delay < time) {
         hueShift = (hueShift + 0.02) % 1;
